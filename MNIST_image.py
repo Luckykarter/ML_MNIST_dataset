0c74026a49b843aad82ca42b8d34aef0bb34847f
Egor Wexler <ferotex7@gmail.com>
2020-06-14 17:31:41 +0300

    MNIST dataset image recognition

---
+++ MNIST_image.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import numpy as np
+np.set_printoptions(linewidth=200)
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# if we want to stop training at some point - there is callback
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        loss = 0.4
+        if(logs.get('loss') < loss):
+            print('\nReached ' + str((1-loss) * 100) +'% accuracy so cancelling training')
+            self.model.stop_training = True
+
+callbacks = myCallback()
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels), \
+(test_images, test_labels) = mnist.load_data()
+
+# print(training_labels[0])
+# print(training_images[0])
+#
+# plt.imshow(training_images[0])
+# plt.show()
+
+# if we training a neural network - it is easier
+# with values from 0 to 1
+# this process called normalizing
+
+# divide entire arrays
+training_images = training_images / 255.0
+test_images = test_images / 255
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+# the number of neurons in the last layer should match the
+# number of classes you are classifying for
+
+# Sequential - defines a SEQUENCE of layers in the neural network
+# Flatten - flattens the square (make 1-dimensional set)
+# Dense - adds a layer of neurons
+#          activation_function tells them what to do
+# Relu - "if X>0 return X else return 0" passes 0 or greater to the next layer
+# Softmax - takes a set and picks the biggest one.
+
+model.compile(optimizer=tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+# train network
+model.fit(training_images, training_labels, epochs=1000,
+          callbacks=[callbacks])
+
+print("evaluate on unseen data:")
+model.evaluate(test_images, test_labels)
+
+print("exercise 1")
+classifications = model.predict(test_images)
+# the value is the probability that the item is each of the 10 classes
+print([str(round(x*100, 3)) + "%" for x in classifications[0]])
